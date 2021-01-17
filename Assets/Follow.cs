3c60cc9b32b5eac9a5dcbb9bd19267389ce3df4d
Laurianelo <32616344+Laurlo@users.noreply.github.com>
2021-01-17 17:42:24 +0100

    movements + camFollow

---
+++ Assets/Follow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+
+    public GameObject target;
+
+    public float smoothTime = 0.3f;
+    public float yOffset;
+
+    private float offsetCamZ = -10f;
+    private Vector2 velocity = Vector2.zero;
+   
+
+    private void Update()
+    {
+        Vector2 targetPos = target.transform.TransformPoint(new Vector3(0, yOffset));
+
+        if(targetPos.y < transform.position.y)
+        {
+            return;
+        }
+
+        targetPos = new Vector2(0, targetPos.y);
+        transform.position = Vector2.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y, offsetCamZ);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float jumpForce;
+    private bool isDraging = false;
+    private Vector2 touchPos;
+    private Vector2 playerPos;
+    private Vector2 dragPos;
+    public float gravity = 1f;
+    public float jumpHeight = 10f;
+     
+    //to improve 
+    private float boundXLefy = -5.4f; 
+    private float boundXRight = 5.4f; 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddGravity();
+        GetInput();
+        MovePlayer();
+
+    }
+
+    private void MovePlayer()
+    {
+        if(isDraging == true)
+        {
+            dragPos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            transform.position = new Vector2(playerPos.x + (dragPos.x - touchPos.x), transform.position.y);
+
+            if(transform.position.x < boundXLefy)
+            {
+                transform.position = new Vector2(boundXLefy, transform.position.y);
+            }
+            else if (transform.position.x > boundXRight)
+            {
+                transform.position = new Vector2(boundXRight, transform.position.y);
+            }
+        }
+    }
+
+    private void GetInput()
+    {
+       if(Input.GetMouseButtonDown(0))
+        {
+            isDraging = true;
+            touchPos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            playerPos = transform.position;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isDraging = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("step"))
+        {
+            if(rb.velocity.y <= 0f)
+            {
+                jumpForce = gravity * jumpHeight;
+                 rb.velocity = new Vector2(0, jumpForce);
+            }
+           
+        }
+    }
+
+    void AddGravity()
+    {
+      // rb.gravityScale = gravity;
+       rb.velocity = new Vector2(0, rb.velocity.y - (gravity * gravity));
+    }
+
+  
+}
